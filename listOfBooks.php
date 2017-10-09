59c5bab937911c6d4bceec9ad74ad9cf34b0c938
Marcus Linné <marcus.linne@lund.ntig.se>
2017-10-09 11:00:09 +0200

    added a twig file that reads and writes out all information from the database it is instructed to do so far

--- listOfBooks.php
+++ listOfBooks.php
@@ -6,14 +6,24 @@
 	<body>
     
 <?php
-   $db = new PDO('mysql:host=localhost;dbname=trälleborg;charset=utf8mb4', 'root', ''); 
-$stmt = $db->query('SELECT * FROM books');
 
-while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-	echo '<tr>';
-    echo "<td>{$row['title']}</td>";
-    echo '<td>'.$row['ISBN'].'</td>';
-	echo '<td>'.$row['author'].'</td>'; 
-	echo '</tr>';
-}
-    ?>
+	require __DIR__ . '/vendor/autoload.php';
+	$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
+	$twig = new Twig_Environment($loader, array('cache' => __DIR__ . '/cache', 'debug' => true));
+	$pdo = new PDO('mysql:host=localhost;dbname=trälleborg;charset=utf8mb4', 'root', '');
+	$stmt = $pdo->query('SELECT * FROM books');
+
+ while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {   
+    $book[] = array(
+        'title'=> $row["title"],
+        'ISBN'=> $row["ISBN"],
+        'author'=> $row["author"],
+        'category'=> $row["category"],
+        'release_date'=> $row["release_date"],
+    	'publisher'=> $row["publisher"],
+    	'language' => $row["language"]
+    );
+	}
+	
+	echo $twig->render('site.twig', array('books' => $book));
+?>
