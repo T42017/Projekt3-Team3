4ed049dd277db858553918292597f402a6af7d41
Simon Svensson <simon.svensson@lund.ntig.se>
2017-10-11 11:08:14 +0200

    fixed alot of stuff

--- index.php
+++ index.php
@@ -13,43 +13,54 @@
         $file = GetPermaLink(1);
         
         if ($file === "admin2") {
-            $db = new PDO('mysql:host=localhost;dbname=trälleborg;charset=utf8mb4', 'root', '');
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $adminFile = GetPermaLink(2);
+            if ($adminFile === "lanaut") {
+                echo $twig->render('admin/lanaUtBocker.twig', array());
+            } else if ($adminFile === "addbook") {
+                $db = new PDO('mysql:host=localhost;dbname=trälleborg;charset=utf8mb4', 'root', '');
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            try {
-                if(isset($_POST['submit'])) {
+                try {
+                    if(isset($_POST['submit'])) {
 
-                    $title = $_POST['title'];
-                    $isbn = str_replace("-", "", $_POST['ISBN']);
-                    $author = $_POST['author'];
-                    $category = $_POST['category'];
-                    $release_year = $_POST['release_year'];
-                    $publisher = $_POST['publisher'];
-                    $language = $_POST['language'];
+                        $title = $_POST['title'];
+                        $isbn = str_replace("-", "", $_POST['ISBN']);
+                        $author = $_POST['author'];
+                        $category = $_POST['category'];
+                        $release_year = $_POST['release_year'];
+                        $publisher = $_POST['publisher'];
+                        $language = $_POST['language'];
 
-                    $stmt = $db->prepare("INSERT INTO books (title, ISBN, author, category, release_year, publisher, language) 
-                                            value (:title, :isbn, :author, :category, :release_year, :publisher, :language)");
+                        $stmt = $db->prepare("INSERT INTO books (title, ISBN, author, category, release_year, publisher, language) 
+                                                value (:title, :isbn, :author, :category, :release_year, :publisher, :language)");
 
-                    $stmt->bindParam(':title', $title);
-                    $stmt->bindParam(':isbn', $isbn);
-                    $stmt->bindParam(':author', $author);
-                    $stmt->bindParam(':category', $category);
-                    $stmt->bindParam(':release_year', $release_year);
-                    $stmt->bindParam(':publisher', $publisher);
-                    $stmt->bindParam(':language', $language);
-                    $stmt->execute();
+                        $stmt->bindParam(':title', $title);
+                        $stmt->bindParam(':isbn', $isbn);
+                        $stmt->bindParam(':author', $author);
+                        $stmt->bindParam(':category', $category);
+                        $stmt->bindParam(':release_year', $release_year);
+                        $stmt->bindParam(':publisher', $publisher);
+                        $stmt->bindParam(':language', $language);
+                        $stmt->execute();
 
-                    //print_r($stmt->fetch(PDO::FETCH_ASSOC));
+                        //print_r($stmt->fetch(PDO::FETCH_ASSOC));
 
-                    echo 'Book tillagd';
+                        echo 'Book tillagd';
+                    }
+                } catch (Exception $e) {
+                    $error = $e->getMessage();
+                    if (strpos($error, 'Duplicate entry') && strpos($error, "for key 'ISBN'")) {
+                        $error = 'En bok med samma ISBN finns redan';
+                    }
                 }
-            } catch (Exception $e) {
-                $error = $e->getMessage();
-                if (strpos($error, 'Duplicate entry') && strpos($error, "for key 'ISBN'")) {
-                    $error = 'En bok med samma ISBN finns redan';
-                }
+                echo $twig->render('admin/addNewBooks.twig', array('error' => isset($error) ? $error : ""));
+            } else if ($adminFile === "lamnain") {
+                echo $twig->render('admin/lamnain.twig', array());
+            } else if ($adminFile === "tabort") {
+                echo $twig->render('admin/tabort.twig', array());
+            } else {
+                echo $twig->render('admin/admin.twig', array());
             }
-            echo $twig->render('admin/addNewBooks.twig', array('error' => isset($error) ? $error : ""));
         } else {
             $pdo = new PDO('mysql:host=localhost;dbname=trälleborg;charset=utf8mb4', 'root', '');
             $stmt = $pdo->query('SELECT * FROM books');
