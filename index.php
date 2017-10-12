a2bcf80a5bd64243300ca4964399660a89497e95
Simon Svensson <simon.svensson@lund.ntig.se>
2017-10-12 14:13:56 +0200

    Working

--- index.php
+++ index.php
@@ -28,28 +28,20 @@
             echo $twig->render('admin/admin.twig', array());
         }
     } else if ($file === "bookinfo") {
-            
         
         require_once 'php/bookinfo.php';
         DoStuff($twig);
     } else {
         $shouldSelectSida = GetPermaLink(2);
         $sida = 1;
-
-<Konflikter vid sammanfogning>
-                       
+        
         if ($shouldSelectSida === 'sida') {
              $sida = GetPermaLink(3);
         }
         $listStart = ($sida - 1) * 18;
         
-        $pdo = ConnectToDatabase();
-        
-        $stmt = $pdo->prepare("SELECT * FROM books ORDER BY id ASC LIMIT :start, 18");
-        $stmt->bindParam(':start', $listStart, PDO::PARAM_INT);
-        $stmt->execute();
-        
-        echo $twig->render('site.twig', array('books' => GetAllBooks($db), 'sida' => $sida));
+        $db = ConnectToDatabase();
+        echo $twig->render('site.twig', array('books' => GetAllBooks($db, $listStart), 'sida' => $sida));
 
     }
 
@@ -77,9 +69,11 @@
         return $db;
     }
 
-    function GetAllBooks($db) {
-        $stmt = $db->query('SELECT * FROM books');
-
+    function GetAllBooks($db, $listStart) {
+        $stmt = $db->prepare('SELECT * FROM books ORDER BY id ASC LIMIT :start, 18');
+        $stmt->bindParam(':start', $listStart, PDO::PARAM_INT);
+        $stmt->execute();
+        
         while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {   
             $book[] = array(
                 'title'=> $row["title"],
