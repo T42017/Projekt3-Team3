50e0ff3b4806e818d1911fb08513f0ef332240e2
Marcus Linné <marcus.linne@lund.ntig.se>
2017-10-19 10:42:32 +0200

    adding images confirmed working

--- tr__lleborg.sql
+++ tr__lleborg.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 19 okt 2017 kl 10:24
+-- Tid vid skapande: 19 okt 2017 kl 10:41
 -- Serverversion: 10.1.19-MariaDB
 -- PHP-version: 7.0.13
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Databas: `trälleborg`
 --
+CREATE DATABASE IF NOT EXISTS `trälleborg` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `trälleborg`;
 
 -- --------------------------------------------------------
 
@@ -78,7 +80,135 @@ INSERT INTO `books` (`id`, `title`, `ISBN`, `author`, `release_year`, `user_id`,
 (61, 'Trafikens grunder', '9789188377159', '', '2016', NULL, 'STR service', 'Svenska', 'uploads/default.png'),
 (62, 'Masse : helvetet tur & retur', '9789188483089', 'Marcus Birro, Mats Magnusson', '2017', NULL, 'Idrottsförlaget', 'Svenska', 'uploads/default.png'),
 (63, 'Creating a website [Elektronisk resurs]', '7309860800007', 'MacDonald, Matthew.', '2011', NULL, 'Sebastopol, Calif. : O''Reilly', 'Engelska', 'uploads/default.png'),
-(85, 'En dtestpost', '1234567890', '', '2000', NULL, 'Huddinge : Förf', 'Svenska', 'uploads/phpFE5.jpg');
+(86, 'Left hand of god', '9780141333557', 'Hoffman, Paul', '2011', NULL, 'Penguin Books Ltd', 'Engelska', 'uploads/php46A6.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `books_genre`
+--
+
+DROP TABLE IF EXISTS `books_genre`;
+CREATE TABLE `books_genre` (
+  `id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `genre_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumpning av Data i tabell `books_genre`
+--
+
+INSERT INTO `books_genre` (`id`, `book_id`, `genre_id`) VALUES
+(85, 1, 8),
+(82, 1, 10),
+(84, 1, 11),
+(50, 2, 2),
+(51, 3, 2),
+(52, 4, 2),
+(53, 5, 2),
+(54, 6, 2),
+(55, 7, 2),
+(56, 8, 2),
+(116, 9, 7),
+(57, 37, 2),
+(86, 37, 8),
+(58, 38, 2),
+(87, 38, 8),
+(59, 39, 2),
+(88, 39, 8),
+(60, 40, 2),
+(89, 40, 8),
+(121, 42, 5),
+(117, 43, 7),
+(119, 44, 9),
+(61, 46, 2),
+(90, 46, 8),
+(62, 47, 2),
+(91, 47, 8),
+(63, 48, 2),
+(92, 48, 8),
+(64, 49, 2),
+(93, 49, 8),
+(65, 50, 2),
+(94, 50, 8),
+(66, 51, 2),
+(95, 51, 8),
+(67, 52, 2),
+(96, 52, 8),
+(68, 53, 2),
+(97, 53, 8),
+(69, 54, 2),
+(98, 54, 8),
+(70, 55, 2),
+(99, 55, 8),
+(71, 56, 2),
+(100, 56, 8),
+(72, 57, 2),
+(101, 57, 8),
+(118, 62, 7),
+(122, 63, 6),
+(123, 65, 6),
+(124, 66, 6),
+(126, 67, 3),
+(129, 67, 5),
+(130, 67, 6),
+(125, 67, 7),
+(127, 67, 9),
+(128, 67, 10),
+(131, 86, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `genre`
+--
+
+DROP TABLE IF EXISTS `genre`;
+CREATE TABLE `genre` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumpning av Data i tabell `genre`
+--
+
+INSERT INTO `genre` (`id`, `name`) VALUES
+(11, 'Action'),
+(8, 'Äventyr'),
+(7, 'Biografi'),
+(3, 'Drama'),
+(2, 'Fantasy'),
+(9, 'Historia'),
+(10, 'Sci-fi'),
+(5, 'Skräck'),
+(6, 'Teknik');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telephone` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumpning av Data i tabell `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `telephone`) VALUES
+(2, 'Simon Svensson', 'nope@nope.com', '0706334793'),
+(3, 'Awesome Simon', 'gmail@gmail.com', '0703667523'),
+(4, 'Erik Eriksson', 'erik.eriksson@gmail.com', '123123456'),
+(5, 'Marcus Linné', 'marcus.linne@lund.ntig.se', '3548654723'),
+(6, 'Mathias Andersson', 'mathias.andersson@lund.ntig.se', '112');
 
 --
 -- Index för dumpade tabeller
@@ -93,6 +223,26 @@ ALTER TABLE `books`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Index för tabell `books_genre`
+--
+ALTER TABLE `books_genre`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `book_id` (`book_id`,`genre_id`);
+
+--
+-- Index för tabell `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Index för tabell `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT för dumpade tabeller
 --
 
@@ -100,7 +250,22 @@ ALTER TABLE `books`
 -- AUTO_INCREMENT för tabell `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
+--
+-- AUTO_INCREMENT för tabell `books_genre`
+--
+ALTER TABLE `books_genre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
+--
+-- AUTO_INCREMENT för tabell `genre`
+--
+ALTER TABLE `genre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT för tabell `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- Restriktioner för dumpade tabeller
 --
