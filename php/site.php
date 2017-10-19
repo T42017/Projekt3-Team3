2a2cf57071e36e1fe6deb0e5eba1aac7f74eba94
Simon Svensson <simon.svensson@lund.ntig.se>
2017-10-19 11:35:27 +0200

    Started working on page numbers in site.twig

--- php/site.php
+++ php/site.php
@@ -1,14 +1,14 @@
 <?php
 function DoStuff($twig) {
     $shouldSelectSida = GetPermaLink(2);
-    $sida = 1;
+    $page = 1;
     $pageSize = 18;
     
     if ($shouldSelectSida === 'sida') {
-        $sida = GetPermaLink(3);
+        $page = GetPermaLink(3);
     }
     
-    $listStart = ($sida - 1) * $pageSize;
+    $listStart = ($page - 1) * $pageSize;
     
     if ($listStart < 0) {
         echo 'Index out of range';
@@ -17,16 +17,11 @@ function DoStuff($twig) {
        
     $db = ConnectToDatabase();
     
-    $goNextPage = false;
-    
-    if ($listStart + $pageSize < GetMaxBooks($db))
-        $goNextPage = true;
-    
     $books = array();
         
     if(isset($_GET['categories']))
     {
-        $categories =$_GET['categories'];
+        $categories = $_GET['categories'];
     } else {
         $categories = '';
     }
@@ -40,46 +35,54 @@ function DoStuff($twig) {
         $search = '';
     }
     
-    
-       
-    echo $twig->render('site.twig', array('books' => GetBooks($db, $listStart, $pageSize, $searchDb, $categories), 'sida' => $sida, 'canGoNextPage' => $goNextPage, 'search' => $search, 'categories' => $categories));
+    echo $twig->render('site.twig', array('books' => GetBooks($db, $listStart, $pageSize, $searchDb, $categories),
+                                          'page' => $page,
+                                          'maxPages' => ceil(GetMaxBooks($db, $searchDb, $categories) / $pageSize),
+                                          'search' => $search,
+                                          'categories' => $categories)
+                      );
 }
- function GetGenresFromBook($db, $isbn) {
-        $stmt = $db->prepare("SELECT genre.name FROM genre
-                                INNER JOIN books_genre 
-                                INNER JOIN books
-                                WHERE ISBN = :isbn AND
-                                books_genre.book_id = books.id AND
-                                books_genre.genre_id = genre.id");
-        $stmt->bindParam(':isbn', $isbn);
-        $stmt->execute();
-        
-        $genres = '';
-        while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            if (!empty($genres))
-                $genres .= ', ';
-            $genres .= $row['name'];
-        }
-        
-        return $genres;
+
+function GetGenresFromBook($db, $isbn) {
+    $stmt = $db->prepare("SELECT genre.name FROM genre
+                            INNER JOIN books_genre 
+                            INNER JOIN books
+                            WHERE ISBN = :isbn AND
+                            books_genre.book_id = books.id AND
+                            books_genre.genre_id = genre.id");
+    $stmt->bindParam(':isbn', $isbn);
+    $stmt->execute();
+
+    $genres = '';
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        if (!empty($genres))
+            $genres .= ', ';
+        $genres .= $row['name'];
+    }
+
+    return $genres;
 }
 
 function GetBooks($db, $listStart, $pageSize, $search, $cat) {
     
     if($cat > 0)
     {
-        $stmt = $db->prepare("SELECT * FROM books INNER JOIN books_genre ON books.id=books_genre.book_id AND books_genre.genre_id=:cat order by books.id");
+        $stmt = $db->prepare("SELECT * FROM books
+                            INNER JOIN books_genre ON books.id = books_genre.book_id AND
+                            books_genre.genre_id = :cat
+                            ORDER BY books.title"
+                            );
         $stmt->bindParam(':cat', $cat, PDO::PARAM_INT);
         $stmt->execute();
         
     } else {
-        $stmt = $db->prepare("SELECT * FROM books WHERE title LIKE :search ORDER BY title ASC LIMIT :start, :pageSize ");
+        $stmt = $db->prepare("SELECT * FROM books WHERE title LIKE :search ORDER BY books.title ASC LIMIT :start, :pageSize ");
         $stmt->bindParam(':start', $listStart, PDO::PARAM_INT);
         $stmt->bindParam(':pageSize', $pageSize, PDO::PARAM_INT);
         $stmt->bindParam(':search', $search, PDO::PARAM_STR);
         $stmt->execute();
     }
-        
+    
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {   
         $book[] = array(
             'title'=> $row["title"],
@@ -95,8 +98,17 @@ function GetBooks($db, $listStart, $pageSize, $search, $cat) {
     return isset($book) ? $book : null;
 }
 
-function GetMaxBooks($db) {
-    $stmt = $db->query('SELECT COUNT(*) FROM books');
+function GetMaxBooks($db, $search, $cat) {
+    if ($cat > 0) {
+        $stmt = $db->prepare('SELECT COUNT(*) FROM books INNER JOIN books_genre ON books.id = books_genre.book_id AND books_genre.genre_id = :cat');
+        $stmt->bindParam(':cat', $cat, PDO::PARAM_INT);
+        $stmt->execute();
+    } else {
+        $stmt = $db->prepare('SELECT COUNT(*) FROM books WHERE title LIKE :search');
+        $stmt->bindParam(':search', $search, PDO::PARAM_STR);
+        $stmt->execute();
+    }
+    
     $row = $stmt->fetch();
     return $row[0];
 }
