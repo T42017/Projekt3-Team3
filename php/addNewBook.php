a681345d458b286c17534ddc8ac24d11860d9323
Simon Svensson <simon.svensson@lund.ntig.se>
2017-10-19 10:17:07 +0200

    fixed small things

--- php/addNewBook.php
+++ php/addNewBook.php
@@ -84,8 +84,6 @@ class Site {
         $this->post['image_location'] = $target_file;
         
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        } else {
             echo "Sorry, there was an error uploading your file.";
         }
     }
